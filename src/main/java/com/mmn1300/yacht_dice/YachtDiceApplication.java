7263b0ebbbe933ff6fa270891f16947ea4d1a6e7
mmn1300 <alsry3904@gamil.com>
2025-08-01 22:01:35 +0900

    최초 커밋

---
+++ src/main/java/com/mmn1300/yacht_dice/YachtDiceApplication.java
@@ -0,0 +1,13 @@
+package com.mmn1300.yacht_dice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YachtDiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YachtDiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmn1300/yacht_dice/YachtDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmn1300.yacht_dice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachtDiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
